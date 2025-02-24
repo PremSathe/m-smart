86fcb90345677e31846a4131c2491fbed5641458
PremSathe <premsathe78@gmail.com>
2025-02-24 16:38:47 +0530

    login page update

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -8,13 +8,13 @@ const LandingPage = () => {
       {/* Wave effects */}
       <div className="wave wave-1"></div>
       <div className="wave wave-2"></div>
-      
+
       {/* Main content */}
       <div className="content">
         <h1>IT Deliberation starts here. For both commercial and personal</h1>
         <button className="get-start-btn">Get Start</button>
       </div>
-      
+
       {/* Scroll indicator */}
       <div className="scroll-indicator">↓</div>
     </div>
